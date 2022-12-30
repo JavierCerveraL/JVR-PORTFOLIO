35875d2258feaff3d2a3c56659036ad03c036001
Javier Cervera <jvrmgmt@gmail.com>
2022-12-30 15:30:31 -0500

    first commit

---
+++ pages/typed.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
+
+const Typed = () => {
+  return (
+    <div id="typed">
+      <TypeAnimation
+        sequence={[
+          'Welcome, I am Javier', // Types 'One'
+          1000, // Waits 1s
+          'and this is my Portfolio', // Deletes 'One' and types 'Two'
+          2000, // Waits 2s
+          'Click anywhere :)',
+          2900, // Types 'Three' without deleting 'Two'
+        ]}
+        wrapper="h1"
+        cursor={true}
+        repeat={Infinity}
+        style={{
+          fontSize: '2.6rem',
+          fontFamily: 'Good',
+          textShadow: '1px 1px 10px rgba(255,255,255,0.6)',
+        }}
+      />
+    </div>
+  );
+};
+
+export default Typed;
